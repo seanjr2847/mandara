18a6176ba56d483cef80bdc6e14604de4c35265f
머그리 <pmlsh2847@gmail.com>
2024-12-31 16:07:06 +0900

    fix

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { useMandalStore } from "@/store/mandal";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
